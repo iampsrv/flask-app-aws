033b76a227dedd9d571c0a564ef60e5a4f4cfa18
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2023-03-16 23:56:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type = var.instance_type
   ami   = var.ami_id
   count = var.number_of_instances
-  vpc_security_group_ids = ["sg-02d85b5dd5f74c7a7"]
+  vpc_security_group_ids = ["sg-xxxxx"]
   tags = {
     Name = "myec2instance2"
   }
