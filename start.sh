1580bca4a6e72f8e065d7ca04a7f8206f54f7190
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2023-03-16 23:57:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
-apt install python3
-apt install git
-apt install python3-pip
+#!/bin/bash
+sudo apt update -y
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt install python3
+sudo apt install git
+git clone https://github.com/iampsrv/flask-app-aws.git
+cd flask-app-aws
+sudo apt-get -y install python3-pip
 pip install -r req.txt
 python3 main.py
